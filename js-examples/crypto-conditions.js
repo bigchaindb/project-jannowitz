1aeb1fc5af011b9d62df8d08135ecf52e324ed22
manolodewiner <manuel@bigchaindb.com>
2018-06-05 15:54:51 +0200

    update file to bdb 2.0

--- js-examples/crypto-conditions.js
+++ js-examples/crypto-conditions.js
@@ -2,6 +2,7 @@ const Buffer = require('buffer').Buffer
 const BigchainDB = require('bigchaindb-driver')
 const base58 = require('bs58')
 const cryptoconditions = require('crypto-conditions')
+const sha3 = require('js-sha3')
 
 const user1 = new BigchainDB.Ed25519Keypair()
 const user2 = new BigchainDB.Ed25519Keypair()
@@ -11,11 +12,18 @@ const receiver = new BigchainDB.Ed25519Keypair()
 
 const API_PATH = 'http://localhost:9984/api/v1/'
 const conn = new BigchainDB.Connection(API_PATH, {
-    app_id: 'xxxxxxxx',
-    app_key: 'xxxxxxxxxxxxxxxxxxx'
+    app_id: '',
+    app_key: ''
 })
 
-console.log(BigchainDB.Transaction.makeEd25519Condition(user1.publicKey))
+
+function sha256Hash(data) {
+    return sha3.sha3_256
+        .create()
+        .update(data)
+        .hex()
+}
+
 // at the output of the transaction to-be-spent
 // Generate threshold condition 2 out of 3
 const threshold = 2
@@ -25,6 +33,7 @@ const condition3 = BigchainDB.Transaction.makeEd25519Condition(user3.publicKey, 
 
 const thresholdCondition = BigchainDB.Transaction.makeThresholdCondition(threshold, [condition1, condition2, condition3])
 
+console.log(thresholdCondition)
 let output = BigchainDB.Transaction.makeOutput(thresholdCondition);
 output.public_keys = [user1.publicKey, user2.publicKey, user3.publicKey];
 
@@ -36,79 +45,65 @@ const tx = BigchainDB.Transaction.makeCreateTransaction({
     creator.publicKey
 )
 
-
 // Sign the transaction with private keys
 const txSigned = BigchainDB.Transaction.signTransaction(tx, creator.privateKey)
 
 // Send the transaction off to BigchainDB
 conn.postTransactionCommit(txSigned)
-    // .then(() => conn.pollStatusAndFetchTransaction(txSigned.id))
+        //.then(() => conn.pollStatusAndFetchTransaction(createTranfer.id))
     .then(res => {
         console.log('Create Transaction', txSigned.id, 'accepted')
-        // const elem = document.getElementById('lastTransaction')
-        // elem.href = API_PATH + 'transactions/' + txSigned.id
-        // elem.innerText = txSigned.id
-        // btn.disabled = false
+        transfer(txSigned)
     })
-    .then((res)=>{
-
-        let createTranfer = BigchainDB.Transaction.makeTransferTransaction(
-          [{ tx: txSigned,
-            output_index: 0
-         }],
-          [BigchainDB.Transaction.makeOutput(
-                BigchainDB.Transaction.makeEd25519Condition(receiver.publicKey))],
-          {
-              what: "Transfer transaction"
-          }
-        );
-          
-                    let fulfillment1 = BigchainDB.Transaction.makeEd25519Condition(user1.publicKey, false)
-                    let fulfillment2 = BigchainDB.Transaction.makeEd25519Condition(user2.publicKey, false)
-                    let fulfillment3 = BigchainDB.Transaction.makeEd25519Condition(user3.publicKey, false)
-                    fulfillment1.sign(
-                        new Buffer(BigchainDB.Transaction.serializeTransactionIntoCanonicalString(createTranfer)),
-                        new Buffer(base58.decode(user1.privateKey))
-                    );
-                    fulfillment2.sign(
-                        new Buffer(BigchainDB.Transaction.serializeTransactionIntoCanonicalString(createTranfer)),
-                        new Buffer(base58.decode(user2.privateKey))
-                    );
-                    fulfillment3.sign(
-                        new Buffer(BigchainDB.Transaction.serializeTransactionIntoCanonicalString(createTranfer)),
-                        new Buffer(base58.decode(user3.privateKey))
-                    );
-
-                    // 2 out of 3 need to sign the fulfillment. Still condition3 is needed as the "circuit definition" is needed.
-                    // See https://github.com/bigchaindb/cryptoconditions/issues/94
-                    let fulfillment = new cryptoconditions.ThresholdSha256()
-                    fulfillment.threshold = 2
-                    fulfillment.addSubfulfillment(fulfillment1.serializeUri())
-                    fulfillment.addSubfulfillment(fulfillment2.serializeUri())
-                    fulfillment.addSubfulfillment(fulfillment3.serializeUri())
-                    fulfillment.addSubconditionUri(condition3.getConditionUri())
-
-                    //Sign the transaction
-                    const fulfillmentUri = fulfillment.serializeUri()
-                    createTranfer.inputs[0].fulfillment = fulfillmentUri
-
-                    conn.postTransactionCommit(createTranfer)
-                        // .then(() => conn.pollStatusAndFetchTransaction(createTranfer.id))
-                        .then(res => {
-                          console.log('res', res);
-                            // const transfTransaction = document.getElementById('transfTransaction')
-                            // transfTransaction.href = API_PATH + 'transactions/' + createTranfer.id
-                            // transfTransaction.innerText = createTranfer.id
-                            // console.log('Transfer Transaction', createTranfer.id, 'accepted')
-                        })
-
-      })
 
 
+async function transfer(txSigned) {
 
+    let createTranfer = BigchainDB.Transaction.makeTransferTransaction(
+      [{ tx: txSigned,
+        output_index: 0
+     }],
+      [BigchainDB.Transaction.makeOutput(
+            BigchainDB.Transaction.makeEd25519Condition(receiver.publicKey))],
+      {
+          what: "Transfer transaction"
+      }
+    );
+
+    const serializedTransaction = BigchainDB.Transaction.serializeTransactionIntoCanonicalString(createTranfer)
     // at the input of the spending transaction
+    let fulfillment1 = BigchainDB.Transaction.makeEd25519Condition(user1.publicKey, false)
+    const transactionUniqueFulfillment1 = createTranfer.inputs[0].fulfills ? serializedTransaction
+                .concat(createTranfer.inputs[0].fulfills.transaction_id)
+                .concat(createTranfer.inputs[0].fulfills.output_index) : serializedTransaction
+    const transactionHash1 = sha256Hash(transactionUniqueFulfillment1)
+    fulfillment1.sign(Buffer.from(transactionHash1, 'hex'), new Buffer.from(base58.decode(user1.privateKey)))
 
-    //     })
+    
+    let fulfillment2 = BigchainDB.Transaction.makeEd25519Condition(user2.publicKey, false)
+    const transactionUniqueFulfillment2 = createTranfer.inputs[0].fulfills ? serializedTransaction
+                .concat(createTranfer.inputs[0].fulfills.transaction_id)
+                .concat(createTranfer.inputs[0].fulfills.output_index) : serializedTransaction
+    const transactionHash2 = sha256Hash(transactionUniqueFulfillment1)
+    fulfillment2.sign(Buffer.from(transactionHash2, 'hex'), new Buffer.from(base58.decode(user2.privateKey)))
 
+    // 2 out of 3 need to sign the fulfillment. Still condition3 is needed as the "circuit definition" is needed.
+    // See https://github.com/bigchaindb/cryptoconditions/issues/94
+    let fulfillment = new cryptoconditions.ThresholdSha256()
+    fulfillment.threshold = 2
+    fulfillment.addSubfulfillment(fulfillment1.serializeUri())
+    fulfillment.addSubfulfillment(fulfillment2.serializeUri())
+    fulfillment.addSubconditionUri(condition3.getConditionUri())
 
-// }
+	//Sign the transaction
+    const fulfillmentUri = fulfillment.serializeUri()
+    createTranfer.inputs[0].fulfillment = fulfillmentUri
+
+    createTranfer.id = await sha256Hash( BigchainDB.Transaction.serializeTransactionIntoCanonicalString(createTranfer))
+
+    conn.postTransactionCommit(createTranfer)
+        .then(res => {
+            console.log('Transfer Transaction', createTranfer.id, 'accepted')
+        })
+
+}
